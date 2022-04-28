eeb91f771e4a5a4d412670bffbd99b5d8fa5911d
Hoonitang <rlaxogns952@gmail.com>
2022-04-28 14:28:39 +0900

    Refactor: StoryBoard제거 코드로 뷰를 작업하기 위해 StoryBoard를 plist의 storyboard, main.,storyboard파일들을 제거함.

--- RxMemo/ViewController/ViewController.swift
+++ RxMemo/ViewController/ViewController.swift
@@ -2,7 +2,19 @@
 //  ViewController.swift
 //  RxMemo
 //
-//  Created by Taehoon Kim on 2022/04/28.
+//  Created by Taehoon Kim on 2022/04/27.
 //
 
-import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
